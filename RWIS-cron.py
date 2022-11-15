1b1697e9bfcf02f2687b932e84e5ffccefbfc0fa
TJ Turnage <t.j.turnage@gmail.com>
2022-11-15 15:54:20 -0500

    - added Path

--- RWIS-cron.py
+++ RWIS-cron.py
@@ -2,14 +2,16 @@ from datetime import datetime
 import os
 import shutil
 import gzip
+from pathlib import Path
 
 src_path='/cifs/RWIS/midotmet.dat'
+src = Path(src_path)
 dst_dir = '/home/wwwgrr/scripts/rwis'
 now = datetime.utcnow()
 now_str = datetime.strftime(now, "%Y%m%d_%H00")
 dst_fn = "RWIS_" + now_str + ".dat.gz"
 dst_path = os.path.join(dst_dir,dst_fn)
 
-with src_path.open(mode='rb') as f_in:
+with src.open(mode='rb') as f_in:
     with gzip.open(dst_path,'wb') as f_out:
         shutil.copyfileobj(f_in,f_out)
